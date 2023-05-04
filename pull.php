dec7fee41796b8ab88126874303e4a28e11279e0
xetos13 <132562332+xetos13@users.noreply.github.com>
2023-05-04 12:03:36 +0200

    actualizando el primer archivo .php

---
+++ pull.php
@@ -0,0 +1,12 @@
+<?php
+
+$persona = [
+              "nombre" => "Pedro",
+              "apellido" => "Sanchez",
+              "edad" => 50
+          ];
+          
+          foreach ($persona as $valor){
+              echo "$valor <br>";
+          }
+?>
